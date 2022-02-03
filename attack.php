1feae9c0da97d7173227d84a18dd4a5ee0882991
Syed Umer Talal Bukhari <talal.bukhari@binaryit.co>
2022-02-03 15:40:09 +0500

    ready for attack

---
+++ attack.php
@@ -0,0 +1,14 @@
+<?php
+
+for($i=0;  $i<99999999999; $i++){    
+    $curl = curl_init('https://www.imdb.com/'); //Website Link which you want to hit
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($curl,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+    echo $i."<br>";
+}
+
+//$page = curl_exec($curl);
+//echo $page;
+
+
+exit();
